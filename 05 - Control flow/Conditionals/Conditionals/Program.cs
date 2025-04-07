008c883b0f75b053671e92f2e57f6b8fe17990e4
AinurImal <imalainur@gmail.com>
2025-04-07 15:34:09 +0800

    05 - Control Flow

---
+++ 05 - Control flow/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+            
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's Autumn and a season of harvest.");
+                    break;
+                case Season.Spring:
+                    Console.WriteLine("It's Spring and a season of new beginnings.");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's Summer and a season of heat.");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("It's Winter and a season of snow.");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+            }
+        }
+            
+    }
+}

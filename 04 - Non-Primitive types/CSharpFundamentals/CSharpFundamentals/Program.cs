4a1cabd0823ab4617b6a6e77e15baf06636539b9
AinurImal <imalainur@gmail.com>
2025-04-07 15:32:51 +0800

    04 - Non-Primitives Types

---
+++ 04 - Non-Primitive types/CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Imal = new Person();
+            Imal.FirstName = "Imal";
+            Imal.LastName = "Ainur";
+            Imal.Introduce();   
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}

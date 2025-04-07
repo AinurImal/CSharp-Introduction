f238a318ffd10258eda4ffe3d60ac50556b91aea
AinurImal <imalainur@gmail.com>
2025-04-07 15:40:14 +0800

    07 - Working with Dates

---
+++ 07 - Working with Dates/DateTime/DateTime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateTimetest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var dateTime = new DateTime(2021, 1, 1);
+           var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+}

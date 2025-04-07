55d161c2c6048f3a50f76f7eff2f2050d8fc3262
AinurImal <imalainur@gmail.com>
2025-04-07 15:44:06 +0800

    09 - Working with file

---
+++ 09 - Working with File/FileTesttwo/FileTesttwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace FileTesttwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            //var files = Directory.GetFiles(@"C:\Users\Work Purpose\source\Csharp", "*.*", SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //  Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"C:\Users\Work Purpose\source\Csharp", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
